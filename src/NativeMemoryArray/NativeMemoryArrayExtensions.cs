cb457e2461bd48497ddefaf64b1c8fb09982d32b
GXR <gxrsprie@163.com>
2024-08-14 17:39:55 +0800

    add extension

--- src/NativeMemoryArray/NativeMemoryArrayExtensions.cs
+++ src/NativeMemoryArray/NativeMemoryArrayExtensions.cs
@@ -3,7 +3,9 @@
 #if !NETSTANDARD2_0 && !UNITY_2019_1_OR_NEWER
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -38,6 +40,35 @@ namespace Cysharp.Collections
                 await stream.WriteAsync(item, cancellationToken);
                 progress?.Report(item.Length);
             }
+        }
+
+
+        public static List<Stream> SpliteToStreamList(this NativeMemoryArray<byte> buffer, long chunkSize = int.MaxValue)
+        {
+            int partCount = (int)Math.Ceiling((double)buffer.Length / chunkSize);
+            List<Stream> streams = new List<Stream>(partCount);
+
+            for (int i = 0; i < partCount; i++)
+            {
+                long length = chunkSize;
+                if ((i + 1) * chunkSize > buffer.Length)
+                {
+                    length = buffer.Length - i * chunkSize;
+                }
+                var stream = buffer.AsStream(i * chunkSize, length);
+                streams.Add(stream);
+            }
+
+            return streams;
+        }
+
+        public unsafe static T* GetPointer<T>(this NativeMemoryArray<T> arr) where T : unmanaged
+        {
+            if (arr.Length == 0)
+            {
+                return default;
+            }
+            return (T*)Unsafe.AsPointer<T>(ref arr[0]);
         }
     }
 }
